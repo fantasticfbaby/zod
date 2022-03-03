079992829bcedc7768390df153091e71518d5e5c
teefing <tengfeitf@foxmail.com>
2022-03-04 00:22:49 +0800

    chore: first modify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import * as mod from "./external";
-export * from "./external";
-export { mod as z };
+export * from "./external"; // 把内容分开各自导出
+export { mod as z }; // 把所有内容打包成一个对象导出
 export default mod;
